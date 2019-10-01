5ab0a52769f94e1c3d363ad68f820cbb38504e3a
Marko Kolaric <33282650+AskmeKolaric@users.noreply.github.com>
2019-10-01 06:53:15 +0200

    Update db_credential.php

--- private/db_credential.php
+++ private/db_credential.php
@@ -1,7 +1,7 @@
 <?php
 
-    define("DB_SERVER", "localhost");
-    define("DB_USER", "root");
-    define("DB_PASS", "root1234");
-    define("DB_NAME", "globe_bank");
+    define("DB_SERVER", "server_name");
+    define("DB_USER", "username");
+    define("DB_PASS", "pass");
+    define("DB_NAME", "name");
 ?>
